6b606950c491a3f8a2880190098ae89563803829
ZamarShoo <shg.ivan97@gmail.com>
2023-06-16 11:01:00 +0300

    slider

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$('.owl-carousel').owlCarousel({
+    center: true,
+    items:2,
+    loop:true,
+    margin:50,
+    dots: false,
+    responsive:{
+        600:{
+            items:4
+        }
+    }
+});
